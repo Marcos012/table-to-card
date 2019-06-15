d1bc4c3a6624283e1798ab643344d9b26903c172
marcos012 <marcospaulo.ad@gmail.com>
2019-06-14 22:34:49 -0300

    Alterada tabela e utilizado mock para popular colunas

--- src/app/data-table/data-table.component.ts
+++ src/app/data-table/data-table.component.ts
@@ -1,5 +1,7 @@
+import { pessoas } from './../mock/pessoas-mock';
 import { Component, ViewChild } from '@angular/core';
 import { MatTableDataSource, MatSort, MatPaginator } from '@angular/material';
+import { Pessoa } from '../models/pessoa';
 
 @Component({
   selector: 'app-data-table',
@@ -7,17 +9,16 @@ import { MatTableDataSource, MatSort, MatPaginator } from '@angular/material';
   styleUrls: ['./data-table.component.scss']
 })
 export class DataTableComponent {
-  displayedColumns = ['id', 'name', 'progress', 'color'];
-  dataSource: MatTableDataSource<UserData>;
+  displayedColumns = ['id', 'nome', 'idade', 'profissao'];
+  dataSource = new MatTableDataSource(pessoas);
 
-  @ViewChild(MatPaginator, {static: true}) paginator: MatPaginator;
-  @ViewChild(MatSort, {static: true}) sort: MatSort;
+  @ViewChild(MatPaginator, {static: false}) paginator: MatPaginator;
+  @ViewChild(MatSort, {static: false}) sort: MatSort;
 
   constructor() {
-    const users: UserData[] = [];
-    for (let i = 1; i <= 100; i++) { users.push(createNewUser(i)); }
-
-    this.dataSource = new MatTableDataSource(users);
+    // const users: Pessoa[] = pessoas;
+    // console.log(users);
+    // this.dataSource = new MatTableDataSource(users);
     this.dataSource.paginator = this.paginator;
     this.dataSource.sort = this.sort;
   }
@@ -28,33 +29,3 @@ export class DataTableComponent {
     this.dataSource.filter = filterValue;
   }
 }
-
-function createNewUser(id: number): UserData {
-  const name =
-    // tslint:disable-next-line: no-use-before-declare
-    NAMES[Math.round(Math.random() * (NAMES.length - 1))] + ' ' +
-    // tslint:disable-next-line: no-use-before-declare
-    NAMES[Math.round(Math.random() * (NAMES.length - 1))].charAt(0) + '.';
-
-  return {
-    id: id.toString(),
-    name: name,
-    progress: Math.round(Math.random() * 100).toString(),
-    // tslint:disable-next-line: no-use-before-declare
-    color: COLORS[Math.round(Math.random() * (COLORS.length - 1))]
-  };
-}
-
-const COLORS = ['maroon', 'red', 'orange', 'yellow', 'olive', 'green', 'purple',
-  'fuchsia', 'lime', 'teal', 'aqua', 'blue', 'navy', 'black', 'gray'];
-const NAMES = ['Maia', 'Asher', 'Olivia', 'Atticus', 'Amelia', 'Jack',
-  'Charlotte', 'Theodore', 'Isla', 'Oliver', 'Isabella', 'Jasper',
-  'Cora', 'Levi', 'Violet', 'Arthur', 'Mia', 'Thomas', 'Elizabeth'];
-
-export interface UserData {
-  id: string;
-  name: string;
-  progress: string;
-  color: string;
-}
-
---
+++ src/app/mock/pessoas-mock.ts
@@ -0,0 +1,29 @@
+export const pessoas: any = [
+    {
+        id: 10199, nome: 'Marcos Paulo', idade: 19, profissao: 'Desenvolvedor'
+    },
+    {
+        id: 10198, nome: 'João da Silva', idade: 34, profissao: 'Carpinteiro'
+    },
+    {
+        id: 10197, nome: 'Maria Flores', idade: 23, profissao: 'Dentista'
+    },
+    {
+        id: 10196, nome: 'Renata Nogueira', idade: 20, profissao: 'Biologa'
+    },
+    {
+        id: 10195, nome: 'Camila da Silva', idade: 38, profissao: 'Advogada'
+    },
+    {
+        id: 10193, nome: 'Gilberto Giba', idade: 52, profissao: 'Colono'
+    },
+    {
+        id: 10192, nome: 'Mineirinho', idade: 104, profissao: 'Mineiro'
+    },
+    {
+        id: 10189, nome: 'Da Fronteira', idade: 72, profissao: 'Muambeiro'
+    },
+    {
+        id: 10188, nome: 'Tchê', idade: 21 , profissao: 'Gauderio'
+    }
+];
